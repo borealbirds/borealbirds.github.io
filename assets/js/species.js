ee9e516c8eaddfe8006b2703a5c562e9679ab346
Peter Solymos <psolymos@gmail.com>
2019-04-12 12:42:13 -0600

    getting there

--- assets/js/species.js
+++ assets/js/species.js
@@ -1,72 +1,78 @@
-$(document).ready(function(){
-
-  // data based rendering
-  $("#title").html(data[0].display + ' &ndash; ' + data[0].tnice);
-  if (data[0].Keep == true) {
-    $("#tag-si").addClass("is-success");
-    var SI = Math.round(data[0].SI_Est);
-    if (data[0].SI2_Est > 100) {
-      var arrow = '<span class="icon"><i class="fa fa-arrow-up"></i></span>'
-    } else {
-      var arrow = '<span class="icon"><i class="fa fa-arrow-down"></i></span>'
+$(document).ready(function() {
+  $.getJSON('https://borealbirds.github.io/api/v4/species/',function(data){
+    //console.log(data);
+    var species = [];
+    var link = [];
+    for (var i in data.data) {
+      species.push(data.data[i].combo);
+      link.push(data.data[i].id);
     }
-    $("#tag-si").html('<span>' + SI + '%</span> ' + arrow);
-    if (SI > 90) {
-      $("#tag-si").addClass("is-success");
-    } else {
-      if (SI > 80) {
-        $("#tag-si").addClass("is-warning");
-      } else {
-        $("#tag-si").addClass("is-danger");
+    //console.log(species);
+    //console.log(link);
+
+    // typeahead
+    var substringMatcher = function(strs) {
+      return function findMatches(q, cb) {
+        var matches, substringRegex;
+        matches = [];
+        substrRegex = new RegExp(q, 'i');
+        $.each(strs, function(i, str) {
+          if (substrRegex.test(str)) {
+            matches.push(str);
+          }
+        });
+        cb(matches);
+      };
+    };
+  
+    $('#spp-search .typeahead').typeahead({
+      hint: true,
+      highlight: true,
+      minLength: 0
+    }, {
+      name: 'species',
+      source: substringMatcher(species),
+      limit: 1000
+    });
+  
+    $.fn.pressEnter = function(fn) {
+      return this.each(function() {
+        $(this).bind('enterPress', fn);
+        $(this).keyup(function(e){
+          if(e.keyCode == 13){
+            $(this).trigger("enterPress");
+          }
+        })
+      });
+    };
+  
+    function checkSpecies(arr, val) {
+       return arr.some(function(arrVal) {
+         return val === arrVal;
+      });
+    }
+  
+    // this deals with species selection
+    $('#spp-select').pressEnter(function(){
+      var spp = $("#spp-select").val();
+      var OK = checkSpecies(species, spp);
+      if (spp != "" && OK == true) {
+        var id = link[species.indexOf(spp)];
+        console.log("Species selected: " + id);
+//          window.location.href=lnk;
+        $.getJSON('https://borealbirds.github.io/api/v4/species/' + id + "/", function(data){
+          console.log(data);
+          $("#results").html(`
+          <h2 class="title" id="intro">Title</h2>` + data.data.species.common[0] +
+          `<p>
+            Results come here
+          <p>
+          `);
+
+        });
+
       }
-    }
-  } else {
-    $("#SI").hide();
-    $("#results").hide();
-  };
-  if (data[0].model_north == true) {
-    $("#tag-north").addClass("is-success");
-    $("#tag-north").text("Yes");
-  } else {
-    $("#tag-north").addClass("is-danger");
-    $("#tag-north").text("No");
-  };
-  if (data[0].model_south == true) {
-    $("#tag-south").addClass("is-success");
-    $("#tag-south").text("Yes");
-  } else {
-    $("#tag-south").addClass("is-danger");
-    $("#tag-south").text("No");
-  };
-  $("#tag-version").text(data[0].version);
-  $("#tag-hf").text(data[0].hf);
-  $("#tag-veg").text(data[0].veg);
-  if (data[0].Comments == "") {
-    $("#comments").hide();
-  } else {
-    $("#message").text(data[0].Comments);
-  }
+    });
 
-  $("#sppweb").attr("href", "http://species.abmi.ca/pages/species/" + data[0].Taxon + '/' + data[0].SpeciesID + '.html');
-
-  var spp = link.indexOf(data[0].SpeciesID);
-  if (spp < 1) {
-    $("#spp-previous").attr("disabled", true);
-    $("#spp-next").attr("href", "../" + link[spp + 1] + "/index.html");
-    console.log("prev disabled");
-  } else {
-    if (spp > link.length-2) {
-      $("#spp-previous").attr("href", "../" + link[spp - 1] + "/index.html");
-      $("#spp-next").attr("disabled", true);
-      console.log("next disabled");
-    } else {
-      $("#spp-previous").attr("href", "../" + link[spp - 1] + "/index.html");
-      $("#spp-next").attr("href", "../" + link[spp + 1] + "/index.html");
-      console.log("both enabled");
-    }
-  }
-
-
-  $("#sppweb").attr("href", "http://species.abmi.ca/pages/species/" + data[0].Taxon + '/' + data[0].SpeciesID + '.html');
-
-});
+  }); // close json
+}); // close window
