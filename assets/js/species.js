6ff513cc1b4f1f5610f9e2a3d80d47a43025de7c
Peter Solymos <psolymos@gmail.com>
2019-04-15 09:42:55 -0600

    that's it for now

--- assets/js/species.js
+++ assets/js/species.js
@@ -125,6 +125,120 @@ $(document).ready(function() {
       });
 
     }
+    function updateSpecies(data){
+      
+      //console.log(data);
+      var id = data.data.species.id;
+
+      // update headings
+      $("#speciesName").text(data.data.species.combo);
+      $("#speciesFam").text(data.data.species.family);
+      $("#speciesPop").text(Math.round(10*data.data.popsize.ntotal[2])/10 + "M inds.");
+
+      // update map links
+      $("#modalImg1a").attr("src", "http://borealbirds.github.io/api/v4/species/" + id + "/map.png");
+      $("#modalImg1b").attr("src", "http://borealbirds.github.io/api/v4/species/" + id + "/map.png");
+      $("#modalImg2a").attr("src", "http://borealbirds.github.io/api/v4/species/" + id + "/mapwdet.png");
+      $("#modalImg2b").attr("src", "http://borealbirds.github.io/api/v4/species/" + id + "/mapwdet.png");
+      $("#modalImg3a").attr("src", "http://www.borealbirds.ca/files/results/relative_density_estimates/" + id + "_Density_April30_2012.jpg");
+      $("#modalImg3b").attr("src", "http://www.borealbirds.ca/files/results/relative_density_estimates/" + id + "_Density_April30_2012.jpg");
+      $("#modalImg4a").attr("src", "http://www.borealbirds.ca/files/results/habitat_suitability/" + id + "_SUIT_MAX.jpg");
+      $("#modalImg4b").attr("src", "http://www.borealbirds.ca/files/results/habitat_suitability/" + id + "_SUIT_MAX.jpg");
+
+      // population size tabe
+      Table.destroy();
+      Table = $('#popsize').DataTable({
+        data: data.data.popsize.nbybcr,
+        columns: [
+            { title: "BCR" },
+            { title: "Area (x10<sup>6</sup> km<sup>2</sup>)" },
+            { title: "Pop. Size (x10<sup>6</sup>)" },
+            { title: "Pop. Density (/ha)" }
+        ],
+        searching: false, 
+        paging: false, 
+        info: false
+      });
+
+      // nalc plot and bcr selection
+      // 0 is all, rest is 1:11 as in 4:14
+      var bcr = 0;
+      updateFig(data, 0);
+      $("#AllBCR").click(function(){
+        console.log("All BCRs selected for " + id)
+        removeHighlight();
+        $("#bcr4").addClass("is-primary");
+        updateFig(data, 0);
+      }); 
+      $("#bcr4").click(function(){
+        console.log("BCR 4 selected for " + id)
+        removeHighlight();
+        $("#bcr4").addClass("is-primary");
+        updateFig(data, 1);
+      }); 
+      $("#bcr5").click(function(){
+        console.log("BCR 5 selected for " + id)
+        removeHighlight();
+        $("#bcr5").addClass("is-primary");
+        updateFig(data, 2);
+      }); 
+      $("#bcr6").click(function(){
+        console.log("BCR 6 selected for " + id)
+        removeHighlight();
+        $("#bcr6").addClass("is-primary");
+        updateFig(data, 3);
+      }); 
+      $("#bcr7").click(function(){
+        console.log("BCR 7 selected for " + id)
+        removeHighlight();
+        $("#bcr7").addClass("is-primary");
+        updateFig(data, 4);
+      }); 
+      $("#bcr8").click(function(){
+        console.log("BCR 8 selected for " + id)
+        removeHighlight();
+        $("#bcr8").addClass("is-primary");
+        updateFig(data, 5);
+      }); 
+      $("#bcr9").click(function(){
+        console.log("BCR 9 selected for " + id)
+        removeHighlight();
+        $("#bcr9").addClass("is-primary");
+        updateFig(data, 6);
+      }); 
+      $("#bcr10").click(function(){
+        console.log("BCR 10 selected for " + id)
+        removeHighlight();
+        $("#bcr10").addClass("is-primary");
+        updateFig(data, 7);
+      }); 
+      $("#bcr11").click(function(){
+        console.log("BCR 11 selected for " + id)
+        removeHighlight();
+        $("#bcr11").addClass("is-primary");
+        updateFig(data, 8);
+      }); 
+      $("#bcr12").click(function(){
+        console.log("BCR 12 selected for " + id)
+        removeHighlight();
+        $("#bcr12").addClass("is-primary");
+        updateFig(data, 9);
+      }); 
+      $("#bcr13").click(function(){
+        console.log("BCR 13 selected for " + id)
+        removeHighlight();
+        $("#bcr13").addClass("is-primary");
+        updateFig(data, 10);
+      }); 
+      $("#bcr14").click(function(){
+        console.log("BCR 14 selected for " + id)
+        removeHighlight();
+        $("#bcr14").addClass("is-primary");
+        updateFig(data, 11);
+      }); 
+
+    }
+
     // removes class from buttons
     function removeHighlight() {
       $("#AllBCR").removeClass("is-primary");
@@ -141,7 +255,21 @@ $(document).ready(function() {
       $("#bcr14").removeClass("is-primary");
     }
 
-// this deals with species selection
+    // initialize table (needs to be destroyed before changing data)
+    Table = $('#popsize').DataTable({
+      data: [[1,2,3,4],[5,6,7,8]],
+      columns: [
+        { title: "BCR" },
+        { title: "Area (x10<sup>6</sup> km<sup>2</sup>)" },
+        { title: "Pop. Size (x10<sup>6</sup>)" },
+        { title: "Pop. Density (/ha)" }
+      ],
+      searching: false, 
+      paging: false, 
+      info: false
+    });
+
+    // this deals with species selection from autocomplete
     $('#spp-select').pressEnter(function(){
       var spp = $("#spp-select").val();
       var OK = checkSpecies(species, spp);
@@ -151,120 +279,15 @@ $(document).ready(function() {
 
         // show results
         document.getElementById("results").style.display = "block";
+        var sppBack = null;
+        var sppNext = null;
         $.getJSON('https://borealbirds.github.io/api/v4/species/' + id + "/", function(data){
-          console.log(data);
-
-          // update headings
-          $("#speciesName").text(data.data.species.combo);
-          $("#speciesPop").text(Math.round(10*data.data.popsize.ntotal[2])/10 + "M inds.");
-//          $("#speciesPop").text("M inds.");
-//          console.log(data.data.species.ntotal[2]);
-
-          // update map links
-          $("#modalImg1a").attr("src", "http://borealbirds.github.io/api/v4/species/" + id + "/map.png");
-          $("#modalImg1b").attr("src", "http://borealbirds.github.io/api/v4/species/" + id + "/map.png");
-          $("#modalImg2a").attr("src", "http://borealbirds.github.io/api/v4/species/" + id + "/mapwdet.png");
-          $("#modalImg2b").attr("src", "http://borealbirds.github.io/api/v4/species/" + id + "/mapwdet.png");
-          $("#modalImg3a").attr("src", "http://www.borealbirds.ca/files/results/relative_density_estimates/" + id + "_Density_April30_2012.jpg");
-          $("#modalImg3b").attr("src", "http://www.borealbirds.ca/files/results/relative_density_estimates/" + id + "_Density_April30_2012.jpg");
-          $("#modalImg4a").attr("src", "http://www.borealbirds.ca/files/results/habitat_suitability/" + id + "_SUIT_MAX.jpg");
-          $("#modalImg4b").attr("src", "http://www.borealbirds.ca/files/results/habitat_suitability/" + id + "_SUIT_MAX.jpg");
-
-          // population size tabe
-          $('#popsize').DataTable({
-            data: data.data.popsize.nbybcr,
-            columns: [
-                { title: "BCR" },
-                { title: "Area (x10<sup>6</sup> km<sup>2</sup>)" },
-                { title: "Pop. Size (x10<sup>6</sup>)" },
-                { title: "Pop. Density (/ha)" }
-            ],
-            searching: false, 
-            paging: false, 
-            info: false
-          });
-
-          // nalc plot and bcr selection
-          // 0 is all, rest is 1:11 as in 4:14
-          var bcr = 0;
-          updateFig(data, 0);
-          $("#AllBCR").click(function(){
-            console.log("All BCRs selected")
-            removeHighlight();
-            $("#bcr4").addClass("is-primary");
-            updateFig(data, 0);
-          }); 
-          $("#bcr4").click(function(){
-            console.log("BCR 4 selected")
-            removeHighlight();
-            $("#bcr4").addClass("is-primary");
-            updateFig(data, 1);
-          }); 
-          $("#bcr5").click(function(){
-            console.log("BCR 5 selected")
-            removeHighlight();
-            $("#bcr5").addClass("is-primary");
-            updateFig(data, 2);
-          }); 
-          $("#bcr6").click(function(){
-            console.log("BCR 6 selected")
-            removeHighlight();
-            $("#bcr6").addClass("is-primary");
-            updateFig(data, 3);
-          }); 
-          $("#bcr7").click(function(){
-            console.log("BCR 7 selected")
-            removeHighlight();
-            $("#bcr7").addClass("is-primary");
-            updateFig(data, 4);
-          }); 
-          $("#bcr8").click(function(){
-            console.log("BCR 8 selected")
-            removeHighlight();
-            $("#bcr8").addClass("is-primary");
-            updateFig(data, 5);
-          }); 
-          $("#bcr9").click(function(){
-            console.log("BCR 9 selected")
-            removeHighlight();
-            $("#bcr9").addClass("is-primary");
-            updateFig(data, 6);
-          }); 
-          $("#bcr10").click(function(){
-            console.log("BCR 10 selected")
-            removeHighlight();
-            $("#bcr10").addClass("is-primary");
-            updateFig(data, 7);
-          }); 
-          $("#bcr11").click(function(){
-            console.log("BCR 11 selected")
-            removeHighlight();
-            $("#bcr11").addClass("is-primary");
-            updateFig(data, 8);
-          }); 
-          $("#bcr12").click(function(){
-            console.log("BCR 12 selected")
-            removeHighlight();
-            $("#bcr12").addClass("is-primary");
-            updateFig(data, 9);
-          }); 
-          $("#bcr13").click(function(){
-            console.log("BCR 13 selected")
-            removeHighlight();
-            $("#bcr13").addClass("is-primary");
-            updateFig(data, 10);
-          }); 
-          $("#bcr14").click(function(){
-            console.log("BCR 14 selected")
-            removeHighlight();
-            $("#bcr14").addClass("is-primary");
-            updateFig(data, 11);
-          }); 
-
-        });  
+          updateSpecies(data);
+        });
 
       }
     });
+
 
   }); // close json
 }); // close window
