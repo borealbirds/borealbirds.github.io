36b0dc447c38bcb83d4659f90a6c77fdbd040219
Peter Solymos <psolymos@gmail.com>
2020-02-12 22:39:26 -0700

    added methods

---
+++ src/pages/Methods.vue
@@ -0,0 +1,12 @@
+<template>
+  <Layout>
+    <div class="container-inner mx-auto py-16">
+        <p>
+            Methods comes here
+        </p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+</script>
--- src/pages/Species.vue
+++ src/pages/Species.vue
@@ -1,9 +1,12 @@
 <template>
   <Layout>
     <div class="container-inner mx-auto py-16">
+      <!-- <h1 class="text-4xl text-blue-400 font-bold mb-8">Species</h1> -->
       <div v-for="spp in sppList" :key="spp.id" class="mb-5">
         <h2 class="text-2xl font-bold"><g-link :to="'/species/' + spp.id" class="text-copy-primary">{{ spp.english }}</g-link></h2>
         <div class="text-copy-secondary mb-4">
+          <!--<span>{{ spp.french }}</span>
+          <span> &middot; </span>-->
           <span><em>{{ spp.scientific }}</em></span>
           <span> &middot; </span>
           <span>Family {{ spp.family }}</span>
